92d4de4b679e9bc213c088fc3db3ce22555401fe
Joni Aaltonen <joni@aaltointeractive.fi>
2015-07-16 12:04:36 +0300

    privacy policy and ToU

---
+++ app/scripts/controllers/terms.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name connectApp.controller:TermsCtrl
+ * @description
+ * # TermsCtrl
+ * Controller of the connectApp
+ */
+angular.module('connectApp')
+  .controller('TermsCtrl', function ($scope) {
+    
+  });
